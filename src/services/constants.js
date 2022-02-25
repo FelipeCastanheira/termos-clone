1088f192c4aa08ca5397b10442df7407e1f2d5e0
Felipe Castanheira <castanheirafc2@gmail.com>
2022-02-25 19:40:44 -0300

    default array

---
+++ src/services/constants.js
@@ -0,0 +1,18 @@
+export const WORD_LIST = [
+  'bravo',
+  'casal',
+  'pente',
+  'cinto',
+  'bolso',
+  'banco',
+  'carne',
+  'radar',
+  'limpo',
+  'feliz',
+  'caixa',
+  'termo',
+  'terno',
+  'cabra',
+];
+
+export const FIVE_NUMBERS = [1, 2, 3, 4, 5];
