c2ba6fd085b7aec7f79865c01002375566fdcd19
Felipe Castanheira <castanheirafc2@gmail.com>
2022-02-25 19:40:07 -0300

    row of letters component

---
+++ src/components/RowOfLetters.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function RowOfLetters() {
+  return (
+    <section className="word-row">
+      <h2>A</h2>
+      <h2>A</h2>
+      <h2>A</h2>
+      <h2>A</h2>
+      <h2>A</h2>
+    </section>
+  )
+}
+
+export default RowOfLetters;
