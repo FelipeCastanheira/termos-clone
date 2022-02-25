2c76b42ab8da4958ce4530b92580e99670d52d60
Felipe Castanheira <castanheirafc2@gmail.com>
2022-02-25 18:38:32 -0300

    test button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <button type="button" className="c-default">Cor Padrão</button>
+        <button type="button" className="c-used">Cor Usada</button>
+        <button type="button" className="c-almost">Cor Lugar-Errado</button>
+        <button type="button" className="c-correct">Cor Correta</button>
       </header>
     </div>
   );
