3a61c4a7c598bdf2e2fac7fbe5e435fbbb163eeb
Felipe Castanheira <castanheirafc2@gmail.com>
2022-02-26 11:29:44 -0300

    getClassName

--- src/components/RowOfLetters.jsx
+++ src/components/RowOfLetters.jsx
@@ -1,16 +1,16 @@
 import React, { useContext } from "react";
 import PropTypes from 'prop-types';
 import WordContext from "../context/WordContext";
-import { wordClassName } from "../services/functions";
+import { getClassName } from "../services/functions";
 
 function RowOfLetters({ index }) {
-  const { attempts, counter } = useContext(WordContext);
+  const { attempts, counter, data } = useContext(WordContext);
   return (
     <section className="word-row">
       { attempts[index].map((letter, letterIndex) => (
         <h2
           key={ `${letter} +${letterIndex}` }
-          className={ `letter ${wordClassName(index, counter)}` }
+          className={ getClassName(index, counter, letter, letterIndex, data) }
         >
           { letter }
         </h2>
