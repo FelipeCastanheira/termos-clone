7ab7c6eb713bb0eb52d543095c17aeefbe2b7e1f
Felipe Castanheira <castanheirafc2@gmail.com>
2022-02-25 21:48:14 -0300

    new component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header';
+import Keyboard from './components/Keyboard';
 import Main from './components/Main';
 import WordProvider from './context/WordProvider';
 
@@ -14,6 +15,7 @@ function App() {
         <button type="button" className="c-almost">Cor Lugar-Errado</button>
         <button type="button" className="c-correct">Cor Correta</button>
       </header>
+      <Keyboard />
     </WordProvider>
   );
 }
