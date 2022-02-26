8b2f97ff292018265287017a9a3bce8d8b16d6f8
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-26 19:46:16 -0300

    fix: setClassName function

--- src/services/functions.js
+++ src/services/functions.js
@@ -14,12 +14,47 @@ export const wordClassName = (index, counter) => {
   return (subtraction ? 'nextWord' : 'curWord');
 }
 
-export const letterClassName = (letter, letterIndex, correctWord) => {
-  if (letter === correctWord[letterIndex]) {
-    return 'c-correct';
+const almostCorrectLetter = ({ word, colors }, letter) => {
+  let filteredWord = '';
+  let keepRemoving = true;
+  for (let i = 0; i < word.length; i += 1) {
+    if (word[i] === letter && keepRemoving) {
+      keepRemoving = false;
+    } else {
+      filteredWord += word[i];
+    }
   }
-  return (correctWord.includes(letter) ? 'c-almost' : 'c-used');
+  return { word: filteredWord, colors: [...colors, 'c-almost'] }
 }
 
-export const getClassName = (index, counter, letter, letterIndex, correctWord) =>
-  `letter ${wordClassName(index, counter)} ${letterClassName(letter, letterIndex, correctWord)}`;
+export const letterClassName = (letterIndex, correctWord, input) => {
+  const initialResult = input.reduce((acc, letter, index) => {
+    if (letter === correctWord[index]) {
+      return {
+        word: acc.word,
+        colors: [...acc.colors, 'c-correct'],
+      };
+    }
+    return {
+      word: acc.word + correctWord[index],
+      colors: [...acc.colors, 'wrong'],
+    };
+  }, {word: '', colors: []});
+  const finalResult = initialResult.colors.reduce((acc, color, index) => {
+    if (color === 'c-correct') {
+      return {word: acc.word, colors: [...acc.colors, color]};
+    }
+    if (acc.word.includes(input[index])) {
+      return almostCorrectLetter(acc, input[index]);
+    }
+    return {word: acc.word, colors: [...acc.colors, 'c-used']};
+  }, {word: initialResult.word, colors: []});
+  const lettersClassNames = finalResult.colors;
+  return lettersClassNames[letterIndex];
+}
+
+export const getClassName = (index, counter, letterIndex, word, input) => {
+  const firstClassName = wordClassName(index, counter);
+  const sndClassName = letterClassName(letterIndex, word, input);
+  return `letter ${firstClassName} ${sndClassName}`;
+}
