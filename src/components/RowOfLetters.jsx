e36c0de7523162ee3ae3d8d99c27b4ec1840ed5a
Felipe Castanheira <castanheirafc2@gmail.com>
2022-02-25 21:48:44 -0300

    conditional exhibition

--- src/components/RowOfLetters.jsx
+++ src/components/RowOfLetters.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import PropTypes from 'prop-types';
+import WordContext from "../context/WordContext";
 
-function RowOfLetters() {
+function RowOfLetters(index) {
+  const { attempts } = useContext(WordContext);
+  console.log(attempts[0]);
   return (
-    <section className="word-row">
-      <h2>A</h2>
-      <h2>A</h2>
-      <h2>A</h2>
-      <h2>A</h2>
-      <h2>A</h2>
-    </section>
+        <section className="word-row">
+          { attempts[index].split('').map((letter) => (
+            <h2 key={ letter }>{ letter }</h2>
+          ))}
+        </section>
   )
+}
+
+RowOfLetters.propTypes = {
+  index: PropTypes.number.isRequired,
 }
 
 export default RowOfLetters;
